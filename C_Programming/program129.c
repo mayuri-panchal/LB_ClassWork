459986e1908a11a3dea939869dff36f1bcf07675
Mayuri Panchal <panchalmayuri0008@gmail.com>
2025-11-13 23:46:57 +0530

    Add files via upload

---
+++ C_Programming/program129.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int CountEven(int Arr[],int iSize)
+{
+    int iCnt = 0, iCounter = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {   
+        if(Arr[iCnt] % 2 == 0)
+        {
+            iCounter++;
+        }
+        
+    }
+
+    return iCounter;
+}
+
+int main()
+{
+    int iLength = 0, iCnt = 0, iRet = 0;
+    int *ptr = NULL;
+
+    printf("Enter no of elements :");
+    scanf("%d",&iLength);
+
+    ptr = (int *)malloc(iLength * sizeof(int));
+    
+    if(NULL == ptr)  // Industrial way of coding
+    {
+        printf("Unable to allocate memory\n");
+        return -1;
+    }
+
+    printf("Enter the elements : ");
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {   
+       scanf("%d",&ptr[iCnt]);
+    }
+
+    iRet = CountEven(ptr,iLength);   
+
+    printf("No of even elements are : %d\n",iRet);
+
+    free(ptr);
+
+    return 0;
+}
---
+++ C_Programming/program130.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int CountOdd(int Arr[],int iSize)
+{
+    int iCnt = 0, iCounter = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {   
+        if(Arr[iCnt] % 2 == 1)
+        {
+            iCounter++;
+        }
+        
+    }
+
+    return iCounter;
+}
+
+int main()
+{
+    int iLength = 0, iCnt = 0, iRet = 0;
+    int *ptr = NULL;
+
+    printf("Enter no of elements :");
+    scanf("%d",&iLength);
+
+    ptr = (int *)malloc(iLength * sizeof(int));
+    
+    if(NULL == ptr)  // Industrial way of coding
+    {
+        printf("Unable to allocate memory\n");
+        return -1;
+    }
+
+    printf("Enter the elements : ");
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {   
+       scanf("%d",&ptr[iCnt]);
+    }
+
+    iRet = CountOdd(ptr,iLength);   
+
+    printf("No of odd elements are : %d\n",iRet);
+
+    free(ptr);
+
+    return 0;
+}
