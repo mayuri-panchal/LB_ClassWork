3b1c884c33bde1151133d40728e1894ff321985b
Mayuri Panchal <panchalmayuri0008@gmail.com>
2025-11-21 13:03:49 +0530

    Add files via upload

---
+++ Java_Programming/program151.java
@@ -0,0 +1,17 @@
+class Pattern
+{
+    public void Display()
+    {
+        System.out.println("Inside Display");
+    }
+}
+
+class program151 
+{
+    public static void main(String A[])
+    {
+        Pattern pobj = new Pattern();
+        pobj.Display();
+    }
+    
+}
