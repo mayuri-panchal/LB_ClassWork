13b3f6b597d168e446ac503f6ff857f43dc8e636
Mayuri Panchal <panchalmayuri0008@gmail.com>
2025-12-03 13:13:04 +0530

    Add files via upload

---
+++ Java_Programming/program292.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class program292 
+{
+    public static void main(String A[])
+    {
+        int Arr[] = {45,21,90,54,78};
+
+        // This is foreach loop
+        for(int no : Arr)
+        {
+            System.out.println(no);
+        }
+    }
+    
+}
